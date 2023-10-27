f372b654eee7ddaf811570c72efc96b5bd9c7eb1
OmarZavala <143145511+OmarZav2338@users.noreply.github.com>
2023-10-26 20:54:51 -0600

    Ataque
    se cargaron los ataques 1,2,3 en el personaje LARA

--- main.cpp
+++ main.cpp
@@ -47,6 +47,55 @@ private:
     BITMAP *psi5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Salto\\Izquierda\\Salto4.bmp", NULL);
     BITMAP *psi6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Salto\\Izquierda\\Salto5.bmp", NULL);
 
+    //Ataque1 Derecha
+    BITMAP *pad1 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Derecha\\LA11.bmp", NULL);
+    BITMAP *pad2 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Derecha\\LA12.bmp", NULL);
+    BITMAP *pad3 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Derecha\\LA13.bmp", NULL);
+    BITMAP *pad4 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Derecha\\LA14.bmp", NULL);
+    BITMAP *pad5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Derecha\\LA15.bmp", NULL);
+    BITMAP *pad6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Derecha\\LA16.bmp", NULL);
+
+    //Ataque1 Izquierda
+    BITMAP *pai1 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Izquierda\\LA11.bmp", NULL);
+    BITMAP *pai2 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Izquierda\\LA12.bmp", NULL);
+    BITMAP *pai3 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Izquierda\\LA13.bmp", NULL);
+    BITMAP *pai4 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Izquierda\\LA14.bmp", NULL);
+    BITMAP *pai5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Izquierda\\LA15.bmp", NULL);
+    BITMAP *pai6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque1\\Izquierda\\LA16.bmp", NULL);
+
+    //Ataque2 Derecha
+    BITMAP *pbd1 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Derecha\\LA21.bmp", NULL);
+    BITMAP *pbd2 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Derecha\\LA22.bmp", NULL);
+    BITMAP *pbd3 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Derecha\\LA23.bmp", NULL);
+    BITMAP *pbd4 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Derecha\\LA24.bmp", NULL);
+    BITMAP *pbd5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Derecha\\LA25.bmp", NULL);
+    BITMAP *pbd6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Derecha\\LA26.bmp", NULL);
+
+    //Ataque2 Izquierda
+    BITMAP *pbi1 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Izquierda\\LA21.bmp", NULL);
+    BITMAP *pbi2 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Izquierda\\LA22.bmp", NULL);
+    BITMAP *pbi3 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Izquierda\\LA23.bmp", NULL);
+    BITMAP *pbi4 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Izquierda\\LA24.bmp", NULL);
+    BITMAP *pbi5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Izquierda\\LA25.bmp", NULL);
+    BITMAP *pbi6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque2\\Izquierda\\LA26.bmp", NULL);
+
+    //Ataque3 Derecha
+    BITMAP *pcd1 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Derecha\\LA31.bmp", NULL);
+    BITMAP *pcd2 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Derecha\\LA32.bmp", NULL);
+    BITMAP *pcd3 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Derecha\\LA33.bmp", NULL);
+    BITMAP *pcd4 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Derecha\\LA34.bmp", NULL);
+    BITMAP *pcd5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Derecha\\LA35.bmp", NULL);
+    BITMAP *pcd6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Derecha\\LA36.bmp", NULL);
+
+    //Ataque3 Izquierda
+    BITMAP *pci1 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Izquierda\\LA31.bmp", NULL);
+    BITMAP *pci2 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Izquierda\\LA32.bmp", NULL);
+    BITMAP *pci3 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Izquierda\\LA33.bmp", NULL);
+    BITMAP *pci4 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Izquierda\\LA34.bmp", NULL);
+    BITMAP *pci5 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Izquierda\\LA35.bmp", NULL);
+    BITMAP *pci6 = load_bitmap("C:\\Users\\Omar Z\\Documents\\GitHub\\Combate-Mortal\\Personajes\\LARA\\Ataque3\\Izquierda\\LA36.bmp", NULL);
+
+
 
 public:
 
@@ -385,6 +434,246 @@ public:
         x=x+j;
         y=345;
         }
+    void ataque1(int pos){
+        blit(buffer, screen,0,0,0,0,1300,450);
+        switch(pos){
+    case 1:
+        blit(pad1,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pad2,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pad3,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pad4,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pad5,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pad6,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+        break;
+
+    case 2:
+        blit(pai1,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pai2,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pai3,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pai4,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pai5,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pai6,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+        break;
+        }
+    }
+    void ataque2(int pos){
+        blit(buffer, screen,0,0,0,0,1300,450);
+        switch(pos){
+    case 1:
+        blit(pbd1,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pbd2,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pbd3,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pbd4,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pbd5,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pbd6,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+        break;
+
+    case 2:
+        blit(pbi1,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pbi2,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pbi3,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pbi4,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pbi5,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pbi6,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+        break;
+        }
+    }
+    void ataque3(int pos){
+        blit(buffer, screen,0,0,0,0,1300,450);
+        switch(pos){
+    case 1:
+        blit(pcd1,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pcd2,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pcd3,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pcd4,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pcd5,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+
+        blit(pcd6,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x+=1;
+        break;
+
+    case 2:
+        blit(pci1,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pci2,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pci3,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pci4,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pci5,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+
+        blit(pci6,screen,0,0,x,y,38,54);
+        rest(120);
+        blit(buffer, screen,0,0,0,0,1300,450);
+
+        x-=1;
+        break;
+        }
+    }
 };
 
 int main ()
@@ -427,7 +716,24 @@ if (key[KEY_UP]&&pos==1){
 if (key[KEY_UP]&&pos==2){
         p1.saltarI();
 }
-
+if (key[KEY_D]){
+        p1.ataque1(pos);
+}
+if (key[KEY_D]){
+        p1.ataque1(pos);
+}
+if (key[KEY_F]){
+        p1.ataque2(pos);
+}
+if (key[KEY_F]){
+        p1.ataque2(pos);
+}
+if (key[KEY_G]){
+        p1.ataque3(pos);
+}
+if (key[KEY_G]){
+        p1.ataque3(pos);
+}
     }
     return 0;
 }
